3625338eedd1bd8da5dcf98f2848b0455f645ccd
nitishree14 <Nitishree14.Supekar@Honeywell.com>
2019-06-06 17:00:57 +0530

    Added commits newest

---
+++ mycalc.java
@@ -0,0 +1,15 @@
+import java.awt.*;  
+import java.awt.event.*;  
+
+public class mycalc extends Frame  
+{  
+  
+public boolean setClear=true;  
+double number;  
+char op;  
+  
+String digitButtonText[] = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0"};  
+String operatorButtonText[] = {"/", "*", "-", "+", "=" };  
+//String memoryButtonText[] = {"MC", "MR", "MS", "M+" };  
+//String specialButtonText[] = {"Backspc", "C", "CE" };  
+  
